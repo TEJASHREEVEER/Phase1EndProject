9a37634bbe19dcfda57fc7dc5928513fa205f40f
VEER Tejashree <Tejashree.VEER@3ds.com>
2023-04-04 15:45:14 +0530

    saving first project

--- src/AmazonTest/AmazonTest.java
+++ src/AmazonTest/AmazonTest.java
@@ -11,13 +11,13 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import com.mysql.cj.jdbc.Driver;
+//import com.mysql.cj.jdbc.Driver;
 import org.openqa.selenium.WebElement;
 
 public class AmazonTest {
 	static WebDriver driver;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
@@ -27,29 +27,34 @@ public class AmazonTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
 
-		WebElement Amzsearch = driver.findElement(By.id("//twotabsearchtextbox"));
-		Amzsearch.sendKeys("Samsung mobiles");
-		driver.findElement(By.id("nav-search-submit-button\""));
+		TakesScreenshot tsObj = (TakesScreenshot) driver;
+		File fileObj = tsObj.getScreenshotAs(OutputType.FILE);
+		File screenshotObj = new File("image.png");
+
+		FileUtils.copyFile(fileObj, screenshotObj);
+
+		WebElement Search = driver.findElement(By.xpath("//input[@placeholder='Search Amazon.in']"));
+		Search.sendKeys("Samsung mobiles");
+		Thread.sleep(2000);
+
+		WebElement btn_search = driver.findElement(By.id("nav-search-submit-button"));
+		btn_search.click();
 	}
 
 	List<WebElement> Product_name = driver.findElements(By.xpath("//div[@class='a-setion']//h2//span"));
 
-	List<WebElement> Product_Price = driver.findElements(By.xpath("//div[@class='a-setion']//a//span[@class='a-price-whole']"));
-	{
-	 
-	for(int i=0;i<Product_name.size();i++)
-			
+	List<WebElement> Product_Price = driver
+			.findElements(By.xpath("//div[@class='a-setion']//a//span[@class='a-price-whole']"));
 
-	
+	List<WebElement> Product_currency = driver
+			.findElements(By.xpath("//div[@class='sg-row']//span[@class='a-price-symbol']"));
 	{
-		System.out.println("Product : " + Product_name.get(i).getText());
-		
-		System.out.println("Price : " +Product_Price.get(i).getText());
+
+		for (int i = 0; i < Product_name.size(); i++) {
+			System.out.println("Product : " + Product_name.get(i).getText());
+
+			System.out.println("Price : " + Product_Price.get(i).getText());
+		}
+
 	}
-
-	TakesScreenshot tsObj = (TakesScreenshot) driver;
-	File fileObj = tsObj.getScreenshotAs(OutputType.FILE);
-	File screenshotObj = new File("image.png");
-
-}
 }
